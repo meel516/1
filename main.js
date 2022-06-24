650c2377a7b294f9020ca1f77d330e937f0bb507
meel516 <102860983+meel516@users.noreply.github.com>
2022-06-24 12:13:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+const saleem='elephant'
+console.log(saleem)    
+const ismail=['apple','ball','cat','dog']
+ ismail[3]= null
+console.log(ismail) 
+
+const todos = [
+    {
+      id: 1,
+      text: 'Take out trash',
+      isComplete: false
+    },
+    {
+      id: 2,
+      text: 'Dinner with wife',
+      isComplete: false
+    },
+    {
+      id: 3,
+      text: 'Meeting with boss',
+      isComplete: true
+    }
+  ];
+ 
+  let modos=[12,12,34,6,74,43]
+ const a=modos.filter(function(todo){
+      
+   
+     return todo
+    
+    }).map(function(redo){
+        console.log(redo)
+        return null});
+ alert ('enter captcha')
+ let b=[] 
+ function salee() {
+    console.log('hello')
+    return [1,2,3]
+    
+ } 
+ 
+ salee()
+ todos.map(salee)    
+ 
+    
+    
+
+  
+  
